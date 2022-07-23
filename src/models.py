afb5c84b9ad2bd8815eecc87c7ee5e535c750b85
alvaro-chiri <alvaro.chirinos@yahoo.com>
2022-07-23 16:11:54 +0000

    final

--- src/models.py
+++ src/models.py
@@ -8,44 +8,44 @@ from eralchemy import render_er
 
 Base = declarative_base()
 
-class Character(Base):
-    __tablename__ = 'character'
-    id = Column(Integer, primary_key=True)
-    name = Column(String)
-    haircolor = Column(String)
-    eyecolor = Column(String)
-
-class Planet(Base):
-    __tablename__ = 'planet' 
-    id = Column(Integer, primary_key=True)
-    name = Column(String)
-    climate = Column(String)
-
 class User(Base):
     __tablename__ = 'user'
     id = Column(Integer, primary_key=True)
     username = Column(String(30), unique=True)
     password = Column(String(16))
-    email = Column(String, unique = True)
+    email = Column(String, unique=True)
     firstname = Column(String)
     lastname = Column(String)
+    description = Column(String, nullable=True)
 
-class Favorite(Base):
-    __tablename__ = 'favorite'
+class Post(Base):
+    __tablename__ = "post"
     id = Column(Integer, primary_key=True)
-    character = relationship(Character)
-    character_id = Column(Integer, ForeignKey('character.id'), nullable=True)
-    planet = relationship(Planet)
-    Planet_id = Column(Integer, ForeignKey('planet.id'), nullable=True)
     user = relationship(User)
     user_id = Column(Integer, ForeignKey('user.id'))
 
-    def validate():
-        if not self.planet_id and not self.character_id:
-            return False
-        return True
+class Media(Base):
+    __tablename__ = "media"
+    id = Column(Integer, primary_key=True)
+    post = relationship(Post)
+    post_id = Column(Integer, ForeignKey('post.id'))
+    url = Column(String)
 
+class Comment(Base):
+    __tablename__ = "comment"
+    id = Column(Integer, primary_key=True)
+    comment_text = Column(String)
+    author = relationship(User)
+    author_id = Column(Integer, ForeignKey('user.id'))
+    post = relationship(Post)
+    post_id = Column(Integer, ForeignKey('post.id'))
 
+class Followers(Base):
+    __tablename__ = 'followers'
+    id = Column(Integer, primary_key=True)
+    user = relationship(User)
+    user_from_id = Column(Integer, ForeignKey('user.id'))
+    user_to_id = Column(Integer, ForeignKey('user.id'))
 
 
 
